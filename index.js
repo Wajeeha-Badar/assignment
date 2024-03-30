a0d4f5c639fe0f99b756830a808f12797300d0b0
wajeehabadar519@gmail.com <wajeehabadar519@gmail.com>
2024-03-30 22:00:10 +0500

    5 assignments

---
+++ index.js
@@ -0,0 +1,20 @@
+//3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var message = "hello world";
+var name13 = ("allama iqbal");
+console.log(name13);
+// step 1 lower case
+var lowerCaseName = name13.toLowerCase();
+console.log(lowerCaseName);
+//step 2 upper case
+var upperCaseName = name13.toUpperCase();
+console.log(upperCaseName);
+// // step 3 title case
+// let word: string[] = name13.split(" ")
+// let tileCaseName : string = 
+// 4: Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+console.log("Allama Iqbal once said,'Destiny is the prison and chain of the ignorant.' ");
+//5: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famousPerson1 = ("Allama Iqbal");
+var message1 = famousPerson1 + " once said, 'The ultimate aim of the ego is not to see something, but to be something'";
+
+console.log(message1);
---
+++ index.ts
@@ -0,0 +1,27 @@
+//3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let message = "hello world";
+let name13: string = ("allama iqbal");
+console.log (name13);
+// step 1 lower case
+let lowerCaseName : string = name13.toLowerCase();
+console.log(lowerCaseName);
+//step 2 upper case
+let upperCaseName : string = name13.toUpperCase();
+console.log(upperCaseName);
+// // step 3 title case
+// let word: string[] = name13.split(" ")
+// let tileCaseName : string = 
+
+
+// 4: Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+console.log("Allama Iqbal once said,'Destiny is the prison and chain of the ignorant.' ");
+//5: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famousPerson1:string = ("Allama Iqbal"); 
+let message1:string= famousPerson1 +" once said, 'The ultimate aim of the ego is not to see something, but to be something'";
+console.log(message1);
+//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so  the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+
+
+
+
