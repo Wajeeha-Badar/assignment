e30db43e6b61f26e8bd23153fba6d7cab9848dc2
wajeehabadar519@gmail.com <wajeehabadar519@gmail.com>
2024-03-30 23:45:44 +0500

    11 assignments done

--- index.js
+++ index.js
@@ -15,6 +15,51 @@ console.log(upperCaseName);
 console.log("Allama Iqbal once said,'Destiny is the prison and chain of the ignorant.' ");
 //5: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
 var famousPerson1 = ("Allama Iqbal");
-var message1 = famousPerson1 + " once said, 'The ultimate aim of the ego is not to see something, but to be something'";
-
+var message1 = "".concat(famousPerson1, " once said, 'The ultimate aim of the ego is not to see something, but to be something'");
 console.log(message1);
+//6: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so  the whitespace around the name is displayed. Then print the name after striping the white spaces.
+var myName = "\t Wajeeha Badar \n";
+console.log("name with whitespace ".concat(myName));
+var strippedName = myName.trim();
+console.log("stripped name :".concat(strippedName));
+//7:Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//Addition
+var num1 = 4;
+var num2 = 4;
+var altogether = num1 + num2;
+var sum = "".concat(num1, " + ").concat(num2, " = ").concat(altogether);
+//Subtraction
+var numb1 = 12;
+var numb2 = 4;
+var left = numb1 - numb2;
+var sub = "".concat(num1, " - ").concat(numb2, " = ").concat(left);
+console.log(sub);
+//Multiplication
+var number1 = 4;
+var number2 = 2;
+var total = number1 * number2;
+var product = "".concat(number1, " * ").concat(number2, " = ").concat(total);
+console.log(product);
+//Division
+var number3 = 72;
+var number4 = 9;
+var result = number3 / number4;
+var result2 = "".concat(number3, " / ").concat(number4, " =").concat(result);
+console.log(result2);
+//8: You should create four lines that look like this: console.log(5 + 3)
+//Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(5 + 3);
+console.log(16 - 8);
+console.log(4 * 2);
+console.log(72 / 9);
+//9:Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favNumber = 6;
+var favNumber1 = "my favourite number is ".concat(favNumber);
+console.log(favNumber1);
+//10:Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//assignment 10 has done.
+//11:Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+var fridendName = ["Ali", "Ahmed", "Raza"];
+console.log(fridendName[0]);
+console.log(fridendName[1]);
+console.log(fridendName[2]);
--- index.ts
+++ index.ts
@@ -17,9 +17,77 @@ console.log(upperCaseName);
 console.log("Allama Iqbal once said,'Destiny is the prison and chain of the ignorant.' ");
 //5: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
 let famousPerson1:string = ("Allama Iqbal"); 
-let message1:string= famousPerson1 +" once said, 'The ultimate aim of the ego is not to see something, but to be something'";
+let message1:string= `${famousPerson1} once said, 'The ultimate aim of the ego is not to see something, but to be something'`
 console.log(message1);
-//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so  the whitespace around the name is displayed. Then print the name after striping the white spaces.
+//6: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so  the whitespace around the name is displayed. Then print the name after striping the white spaces.
+let myName :string = "\t Wajeeha Badar \n";
+console.log(`name with whitespace ${myName}`);
+let strippedName : string = myName.trim();
+console.log(`stripped name :${strippedName}`);
+
+
+
+//7:Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//Addition
+
+let num1 : number = 4;
+let num2 : number = 4;
+let altogether : number = num1 + num2;
+let sum = `${num1} + ${num2} = ${altogether}`;
+
+
+//Subtraction
+let numb1 : number = 12;
+let numb2 : number = 4;
+let left = numb1 - numb2;
+let sub = `${num1} - ${numb2} = ${left}`;
+console.log(sub);
+
+//Multiplication
+let number1 : number = 4;
+let number2 : number = 2;
+let total = number1 * number2;
+let product =`${number1} * ${number2} = ${total}`;
+console.log(product);
+ //Division
+ let number3 : number = 72 ;
+ let number4 : number = 9 ;
+ let result : number = number3 / number4 ;
+ let result2 =`${number3} / ${number4} =${result}`;
+ console.log(result2);
+//8: You should create four lines that look like this: console.log(5 + 3)
+//Your output should simply be four lines with the number 8 appearing once on each line.
+
+ console.log(5 + 3);
+ console.log(16 - 8);
+ console.log(4 * 2);
+ console.log(72 / 9);
+
+ //9:Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+ let favNumber : number = 6
+ let favNumber1 : string = `my favourite number is ${favNumber}`
+ console.log(favNumber1)
+
+ //10:Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+ //assignment 10 has done.
+
+
+ //11:Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+ let fridendName: string[]= ["Ali", "Ahmed","Raza"] ;
+ console.log(fridendName[0]);
+ console.log(fridendName[1]);
+ console.log(fridendName[2]);
+
+ 
+
+
+
+
+
+
+
 
 
 
